5ff156bb512b353f6a589ba64655a79d3cebed33
Umesh76007 <128772309+Umesh76007@users.noreply.github.com>
2023-10-20 15:33:47 +0530

    Add files via upload

---
+++ book.cpp
@@ -0,0 +1,19 @@
+include #include<iostream>
+include "book.h"
+using namespace std;
+
+public class book{
+private:
+	int id=5;
+	int price=500;
+	string name=Blue origin;
+	class(){
+	}
+	
+	 void display()
+	{	
+	  cout<<"id="<<id<<endl;
+	    cout<<"price="<<price<<endl;
+	      cout<<"name="<<name<<endl;
+	  	}
+};
